16c8a900dbef71c83d2acf723be39412c3c07bee
Eric Maibach <Eric@maibachfamily.com>
2020-10-28 21:52:03 -0400

    Updated PairwiseAlignment method to do scoring
    Updated the PairwiseAlignment method to create a scoring matrix.  Still need to update it to use that scoring matrix to generate the correct alignment.  Should also provide the ability to set different scoring values.

--- Pairwise.py
+++ Pairwise.py
@@ -1,10 +1,51 @@
+import numpy as np
+
 def main():
     print("Hello world!")
+    scr = PairwiseAlignment("AGC", "AAAC")
+    print(scr[:,:,0])
+    print(scr[:,:,1])
 
 # Pairwise alignment function
-def PairwiseAlignment():
-
-    return
+def PairwiseAlignment(dna1, dna2):
+    scores = np.zeros(shape=(len(dna1) + 1, len(dna2) + 1, 2))
+    for x in range(0, len(dna1) + 1):
+        for y in range(0, len(dna2) + 1):
+            if x == 0:
+                if y == 0:
+                    scores[x, y, 0] = 0
+                    scores[x, y, 1] = 0
+                else:
+                    scores[x, y, 0] = scores[x, y-1, 0] - 2
+                    scores[x, y, 1] = 1
+            else:
+                if y == 0:
+                    scores[x, y, 0] = scores[x-1, y, 0] - 2
+                    scores[x, y, 1] = 2
+                else:
+                    upscore = scores[x, y-1, 0] - 2
+                    leftscore = scores[x-1, y, 0] - 2
+                    
+                    score = 0
+                    if dna1[x-1] == dna2[y-1]:
+                        score = 1
+                    else:
+                        score = -1
+                        
+                    diagscore = scores[x-1,y-1, 0] + score
+                    
+                    maxscore = max(upscore, leftscore, diagscore)
+                    dir = 0
+                    if maxscore == upscore:
+                        dir = 1
+                    elif maxscore == leftscore:
+                        dir = 2
+                    else:
+                        dir = 3
+                        
+                    scores[x, y, 0] = maxscore
+                    scores[x, y, 1] = dir
+    return scores
 
 # Define entry point
 if __name__ == "__main__":
