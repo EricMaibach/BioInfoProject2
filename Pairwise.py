8eeb1eab2934a8ac394ba76339cef298b58c4beb
Eric Maibach <Eric@maibachfamily.com>
2020-11-04 22:55:35 -0500

    Count Syn, NonSyn, Indel
    Add methods to count syn, nonsyn, and indel mutations

--- Pairwise.py
+++ Pairwise.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 def main():
     # Open both genome fasta files
@@ -16,8 +17,12 @@ def main():
     msars = msars.replace('\n', '')
     mcovid = mcovid.replace('\n', '')
     # Pairwise align both M gene sequences
+
     paired = PairwiseAlignment(msars, mcovid, [1, 0, -1])
     print(paired)
+
+    alignscore = ScoreStrand(paired[0], paired[1])
+    print(alignscore)
 
 # Pairwise alignment function
 def PairwiseAlignment(dna1, dna2, scoring):
@@ -87,6 +92,105 @@ def PairwiseAlignment(dna1, dna2, scoring):
     # print(scores[:,:,1])
 
     return ["".join(strand1), "".join(strand2)]
+
+def ScoreStrand(sarsstrand, covidstrand):
+    score = {
+        "SynCount": 0,
+        "NonsynCount": 0,
+        "IndelCount": 0
+    }
+    atgindexes = [m.start() for m in re.finditer('ATG', covidstrand)]
+    
+    protdict = { 
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
+            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
+            'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+        }
+
+    for indx in atgindexes:
+        strindx = indx
+        while True:
+            sarstrip = sarsstrand[strindx:(strindx + 3)]
+            covtrip = covidstrand[strindx:(strindx + 3)]
+
+            if "_" not in covtrip:
+                if protdict[covtrip] == '_':
+                    break
+
+            tripscore = ScoreTriplet(sarstrip, covtrip)
+            score["SynCount"] = score["SynCount"] + tripscore["SynCount"]
+            score["NonsynCount"] = score["NonsynCount"] + tripscore["NonsynCount"]
+            score["IndelCount"] = score["IndelCount"] + tripscore["IndelCount"]
+            strindx = strindx + 3
+
+    return score
+
+def ScoreTriplet(sarstriplet, covidtriplet):
+    score = {
+        "SynCount": 0,
+        "NonsynCount": 0,
+        "IndelCount": 0
+    }
+    if "_" in covidtriplet:
+        gapindexes = [i for i, ltr in enumerate(covidtriplet) if ltr == "_"]
+        if len(gapindexes) == 3:
+            score["IndelCount"] = 1
+        elif len(gapindexes) == 1:
+            score["IndelCount"] = 1
+        else:
+            if 1 in gapindexes:
+                score["IndelCount"] = 1
+            else:
+                score["IndelCount"] = 2
+    elif "_" in sarstriplet:
+        gapindexes = [i for i, ltr in enumerate(sarstriplet) if ltr == "_"]
+        if len(gapindexes) == 3:
+            score["IndelCount"] = 1
+        elif len(gapindexes) == 1:
+            score["IndelCount"] = 1
+        else:
+            if 1 in gapindexes:
+                score["IndelCount"] = 1
+            else:
+                score["IndelCount"] = 2
+    elif sarstriplet != covidtriplet:
+        protdict = { 
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
+            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
+            'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+        } 
+        if protdict[sarstriplet] != protdict[covidtriplet]:
+            score["SynCount"] = 1
+        else:
+            score["NonsynCount"] = 1
+
+    return score
 
 # Define entry point
 if __name__ == "__main__":
